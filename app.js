9c320416e3514829009d741c26f2117a04c5da11
osonoi <osonoi@hotmail.com>
2025-10-03 21:37:04 +0900

    added hello

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/hello');
+var helloRouter = require('./routes/hello');
 
 
 var app = express();
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
